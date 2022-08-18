09a2b61f87debcdb4a3d773ecc5a0c670db60779
ShivuSFS <100753552+ShivuSFS@users.noreply.github.com>
2022-08-18 13:30:25 +0530

    first Commit

---
+++ frontend/src/component/layout/About/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <img
+        src="https://cdn.dribbble.com/users/2029822/screenshots/4468075/mpd008.png"
+        style={{ height: "100vh", width: "100vw" }}
+      />
+      <h1 style={{ position: "absolute", top: "50%", left: "40%" }}>
+        WORK IN PROGRESS...
+      </h1>
+    </div>
+  );
+}
+
+export default About;
